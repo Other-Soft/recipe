dcb7d7307ee6455f8ba1462e9e2d7781ec7e8823
Sergey Tischenko <sergeytischenko@DeathStar-2.local>
2013-04-08 14:30:52 +0400

    xml

--- src/main/scala/for-xml.scala
+++ src/main/scala/for-xml.scala
@@ -4,8 +4,9 @@ import pro.savant.circumflex._, core._, web._, freemarker._
 import java.io.File
 import java.util.Date
 
-class time (val name: String)
-     extends  with XmlFile {
+
+class Time
+     extends ListHolder[] with XmlFile {
 
   def descriptorFile = new File("src/main/resources/time.xml")
 
